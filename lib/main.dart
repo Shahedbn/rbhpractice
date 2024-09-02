42119869c39606900322827cd06495525d94e40c
Shahidul Islam Shahed <shahedbn751@gmail.com>
2024-09-02 23:14:47 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Enhanced Greeting App'),
+          title: const Text('HUMMING BARD'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Hello, World!',
-                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 40),
+            children: [
+              Text(
+                'FLUTTER WEB.',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const Text('Welcome to Flutter!',
-              style: TextStyle(fontSize: 30),
+              Text(
+                'THE BASICS',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-
+              const SizedBox(height: 16),
+              Text( textAlign: TextAlign.justify,
+                'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals and more',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
               ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Button Pressed!')),
-                  );
-                },
-                child: const Text('Click Me'),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: (){}, child: const Text('Click Me'),
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer (
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  padding: EdgeInsets.all(0),
+                  child: UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(color: Colors.lightBlue,),
+                    accountName: Text('SKILL UP NOW'),
+                    accountEmail: Text('TAP HERE'),
+                  )
+              ),
+              ListTile(
+                title: Text('Episodes'),
+                leading: Icon(Icons.movie_filter_outlined),
+                onTap: (){},
+              ),
+              ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.quiz),
+                onTap: (){},
               ),
             ],
           ),
